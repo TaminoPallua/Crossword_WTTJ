6e084d30423da5d53b7f3165cb9c05297e1adf34
FlanitzerMoritz <125306682+FlanitzerMoritz@users.noreply.github.com>
2024-03-19 11:42:17 +0100

    Frage 1 geändert

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let letters0 = ['F', 'L', 'O', 'R', 'A'];
 let letters1 = ['A', 'B', 'B', 'A', 'U'];
 let letters2 = ['B', 'R', 'A', 'N', 'D'];
 let letters3 = ['A', 'X', 'T'];
+let letters4 = ['']
 
 function sumbit1() {
   let userInput = document.getElementById('submissionText').value.toLowerCase();
